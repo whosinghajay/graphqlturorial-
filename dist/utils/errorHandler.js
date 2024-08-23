8d4f609235b9eb6e668c15a2e66e6b3592564161
unknown <whosinghajay@gmail.com>
2024-08-23 17:33:12 +0530

    .

--- dist/utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export default class ErrorHandler extends Error {
-    constructor(statusCode, message) {
-        super(message);
-        this.statusCode = statusCode;
-        this.message = message;
-        this.statusCode = statusCode;
-    }
-}
--- src/utils/errorHandler.ts
+++
@@ -1,6 +0,0 @@
-export default class ErrorHandler extends Error {
-  constructor(public statusCode: number,public message: string) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
